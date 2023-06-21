02d7105a878a6451a0cbeae1b0ff38f10f5394c3
raulfarce01 <raulito72001@gmail.com>
2023-06-21 19:03:01 +0200

    Fin del ejercicio

--- Angular-Leccion3/Practica1_Leccion3_RaulFdezArce/src/app/app-routing.module.ts
+++ Angular-Leccion3/Practica1_Leccion3_RaulFdezArce/src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CurriculumVitaeComponent } from './pages/curriculum-vitae/curriculum-vitae.component';
+import { FormComponent } from './pages/form/form.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "curriculum-vitae",
+    component: CurriculumVitaeComponent
+  },
+  {
+    path: "form",
+    component: FormComponent
+  },
+  {
+    path: "not-found",
+    component: NotFoundComponent
+  },
+  {
+    path: "**",
+    pathMatch: "full",
+    redirectTo: "not-found"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Angular-Leccion3/Practica1_Leccion3_RaulFdezArce/src/app/app.module.ts
+++ Angular-Leccion3/Practica1_Leccion3_RaulFdezArce/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FormComponent } from './pages/form/form.component';
+import { CurriculumVitaeComponent } from './pages/curriculum-vitae/curriculum-vitae.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    FormComponent,
+    CurriculumVitaeComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
