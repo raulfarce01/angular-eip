d32f81ecde0bd09a6a81c3c0ea50abab72101f82
raulfarce01 <raulito72001@gmail.com>
2023-06-21 09:22:27 +0200

    Documentación y creación del proyecto

---
+++ Angular-Leccion3/Practica1_Leccion3_RaulFdezArce/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Practica1_Leccion3_RaulFdezArce';
+}
